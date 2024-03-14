bf1b4dd1097fdc127607c0509a578e8ca32c64ae
Nguyen Mai Anh Do <anhdodr@ggmail.com>
2024-03-14 13:25:04 -0700

    finish processing varscan

--- bash_script/callvariant_script/varscan.sh
+++ bash_script/callvariant_script/varscan.sh
@@ -5,8 +5,8 @@
 #SBATCH --error=/home/ndo/slurm_script/varscan_%j.err
 
 #
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --nodes=3
+#SBATCH --ntasks=3
 #SBATCH --cpus-per-task=12
 #SBATCH --mem-per-cpu=6G
 
@@ -17,10 +17,16 @@ conda activate /home/ndo/miniconda3/envs/artSeq
 bed=/home/ndo/GIAB-GT/HG001_GRCh38_1_22_v4.2.1_benchmark.bed
 ref=/home/ndo/GIABv3Ref/GRCh38_GIABv3_no_alt_analysis_set_maskedGRC_decoys_MAP2K3_KMT2C_KCNJ18.fasta
 prior=0.99
-bam=/home/ndo/bash_output/03_final_bam/SRR13586007_sortCoord.bam
-out=/home/ndo/bash_output/04_callvariant_vcf/varscan_output.vcf
+bam=/home/ndo/bash_output/05_bedtools_intersect/SRR13586007_bam_intersected.bam
+# out=/home/ndo/bash_output/04_callvariant_vcf/vsc_1.vcf
 #time bcftools mpileup -B -Q5 --max-BQ 30 -I -Ou --threads 28 -T $bed -f $ref $cram | bcftools call -P $prior --threads 28 -mv -Oz -o ~/PAO89685.pass.cram_highconf.vcf
 
 
+ 
+#Try with different coverage
+
+# samtools mpileup -f $ref $bam \
+# | varscan pileup2snp --min-coverage 2 --min-reads2 1 > /home/ndo/bash_output/04_callvariant_vcf/vsc_mincov2.vcf
+
 samtools mpileup -f $ref $bam \
-| varscan pileup2snp --min-coverage 1 --min-reads2 1 > $out
+| varscan pileup2snp --min-coverage 3 --min-reads2 2 > /home/ndo/bash_output/04_callvariant_vcf/vsc_mc3_rs2.vcf
--- bash_script/intersect_script.sh
+++ bash_script/intersect_script.sh
@@ -16,8 +16,8 @@ eval "$(conda shell.bash hook)"
 eval "$(/home/ndo/miniconda3/bin/conda shell.bash hook)"
 conda activate bedtools
 
-### INTERSECT WITH LOFREQ
+### INTERSECT WITH BAM file (ignore the vcf output)
 
-bedtools intersect -header -a /home/ndo/bash_output/04_callvariant_vcf/SRR13586007_lofreq.vcf \
+bedtools intersect -header -a /home/ndo/bash_output/03_final_bam/SRR13586007_sortCoord.bam \
 -b /home/ndo/GIAB-GT/HG001_GRCh38_1_22_v4.2.1_benchmark.bed \
-> /home/ndo/bash_output/05_bedtools_intersect/SRR13586007_intersected.vcf
+> /home/ndo/bash_output/05_bedtools_intersect/SRR13586007_bam_intersected.bam
